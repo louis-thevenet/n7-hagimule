464eab02df7d07236f4c0877911bb6dcd621748c
Louis Thevenet <louis.tvnt@gmail.com>
2024-12-03 08:53:56 +0100

    chore: init Daemon and Diary projects

--- src/main/java/fr/n7/hagimule/App.java
+++
@@ -1,8 +0,0 @@
-package fr.n7.hagimule;
-
-/** Hello world! */
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
---
+++ src/main/java/hagimule/App.java
@@ -0,0 +1,10 @@
+package hagimule;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
