b1791e4286fd9f4451d1a643e8a9fb15f819e3ce
tle2734 <timothe.lebobe@etu.inp-n7.fr>
2025-01-14 01:30:12 +0100

    fix: script don't executable

--- lauch_c304.sh
+++ lauch_c304.sh
@@ -6,12 +6,19 @@ for i in $(seq 1 15); do
 			machine=c304-0$i
 		fi
 		echo machine=$machine
+		echo
 
 		if [ $1 == "downloader" ]; then
 			jarpath=downloader/build/libs/downloader-all.jar
+			p=" $3"
 		else
+			bash -c "ssh $machine cp -r '$HOME/Downloads /work/'"
+			bash -c "ssh $machine ls /work/"
+			echo 
+			echo
 			jarpath=daemon/build/libs/daemon-all.jar
+			p="Downloads"
 		fi
-		bash -c "ssh $machine java -jar $jarpath -dii $2 -p /work/ $3 &"
+		bash -c "ssh $machine java -jar $jarpath -dii $2 -p /work/$p &"
 	fi
 done
